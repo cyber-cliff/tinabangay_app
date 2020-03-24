eafcd3a759fa6839e6054e4e8204115d05bf1d36
Kennette Canales <kennettecanales@gmail.com>
2020-03-24 18:38:15 +0800

    update

--- common/Helper.js
+++ common/Helper.js
@@ -30,6 +30,7 @@ export default {
       colors: [Color.primary, Color.lightGray]
     }
   ],
+  retrieveDataFlag: 1,
   validateEmail(email){
     let reg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+.[a-zA-Z0-9]*$/
     if(reg.test(email) === false){
